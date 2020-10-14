a7f36c553acdf1524e7baee0fde7bbcdca5567d9
kevinxu31 <58008922+kevinxu31@users.noreply.github.com>
2020-10-13 19:11:08 -0700

    Completed part 3

--- turnin/lxu092_lab2_part2.c
+++ turnin/lxu092_lab2_part2.c
@@ -1,7 +1,7 @@
 /*	Author: lxu092
  *  Partner(s) Name: Luofeng Xu
  *	Lab Section:2
- *	Assignment: Lab #  Exercise #
+ *	Assignment: Lab 2  Exercise 2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
